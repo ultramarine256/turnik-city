9ad06a551b2f10030dafe2a0ab36b0c293976d13
Evgeny Platonov <ultramarine256@gmail.com>
2022-08-05 14:57:32 +0300

    ci: added env.PORT for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+
   app.setGlobalPrefix('/');
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
