79e83e5900a353068acd7b04754a5b4a4b7f850d
Dmitry Moiseev <mdmitry94@gmail.com>
2022-08-07 18:01:02 +0300

    refactor: remove unused imports

--- frontend/src/app/location/location.service.ts
+++ frontend/src/app/location/location.service.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { toAsyncState } from '@ngneat/loadoff';
-import { delay, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 export type City = {
