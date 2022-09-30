10ac708718d3dc71ba7cc8974a58265d650feb51
Evgeny Platonov <ultramarine256@gmail.com>
2022-09-30 20:05:33 +0300

    refactor: moved node-api to private repo

--- backend/src/cities/cities.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CitiesController } from './cities.controller';
-import { CitiesService } from './cities.service';
-
-@Module({
-  controllers: [CitiesController],
-  providers: [CitiesService],
-})
-export class CitiesModule {}
