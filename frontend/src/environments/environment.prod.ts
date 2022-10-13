a9a5ffd8508768bb658e721bfdfc8eb5a3922a1e
Evgeny Platonov <ultramarine256@gmail.com>
2022-10-13 14:33:00 +0300

    ci: frontend deploy setup

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   apiBaseUrl: 'https://api.turnik.city',
   googleMapsApiKey: 'google-maps-api-key',
-  googleTrackingCode: 'G-643KF91M6W',
+  googleTrackingCode: '-measurement-id-',
   mapbox: {
-    accessToken: 'pk.eyJ1IjoiYnJhc2thbSIsImEiOiJja3NqcXBzbWoyZ3ZvMm5ybzA4N2dzaDR6In0.RUAYJFnNgOnn80wXkrV9ZA',
+    accessToken: '-access-token-',
   },
 };
