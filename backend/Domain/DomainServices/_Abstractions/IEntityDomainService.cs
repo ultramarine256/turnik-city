48189346a3d65e85b42a640e8e7fd31db6ad67af
brian-boitano <karpenko.oleh.19@gmail.com>
2023-04-23 19:59:34 +0300

    Temporary hard fix.

--- backend/Domain/DomainServices/_Abstractions/IEntityDomainService.cs
+++ backend/Domain/DomainServices/_Abstractions/IEntityDomainService.cs
@@ -5,7 +5,7 @@ namespace Domain.DomainServices._Abstractions
     public interface IEntityDomainService<TEntity, TPrimaryKey>
         where TEntity : class, IEntity<TPrimaryKey>
     {
-        IQueryable<TEntity> Query(int skip = 0, int take = 100);
+        IQueryable<TEntity> Query(int skip = 0, int take = 10000000);
         Task<TEntity> GetAsync(TPrimaryKey id);
         Task<TEntity> CreateAsync(TEntity entity);
         Task<TEntity> UpdateAsync(TEntity entity);
