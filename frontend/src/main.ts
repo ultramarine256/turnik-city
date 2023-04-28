355cc59ac3860fc0b2a6ec030e5b202611903280
Evgeny Platonov <ultramarine256@gmail.com>
2023-04-29 00:57:23 +0300

    feat: removed ssr

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,11 +11,7 @@ if (environment.production) {
 function bootstrap() {
   platformBrowserDynamic()
     .bootstrapModule(AppModule)
-    .catch((err) => console.error(err));
+    .catch(err => console.error(err));
 }
 
-if (document.readyState === 'complete') {
-  bootstrap();
-} else {
-  document.addEventListener('DOMContentLoaded', bootstrap);
-}
+bootstrap();
