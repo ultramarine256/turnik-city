95042abbd2102390b5666cf1cfc8bd03c7236660
Evgeny Platonov <ultramarine256@gmail.com>
2023-09-19 18:25:31 +0300

    feat: bumped `target` to `ES2022`

--- frontend/src/app/data/base.repository.ts
+++ frontend/src/app/data/base.repository.ts
@@ -16,6 +16,7 @@ export abstract class BaseRepository {
     });
   }
 }
+
 export function generateBackground(title: string, fontSize: number = 0.25, size: number = 128) {
   const colors = ['366a25', '2489ce', '8d6d64', 'bc3217', 'ee6b1c', 'aa45b9', 'bd115c', '2197a6', 'f18eb1', '5ad1e0'];
   const color = colors[Math.floor(Math.random() * colors.length)];
